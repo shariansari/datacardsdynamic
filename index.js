759b9cfea51c722f51816eea419dcffc62eb6d09
shariq <ansari.shariq.badshah@gmail.com>
2022-11-17 13:41:27 +0530

    second commit

--- index.js
+++ index.js
@@ -15,6 +15,28 @@ const data =[
         title:"bfdfda",
         rating:"133.22",
     },
+    {
+        title:"bfdfda",
+        rating:"133.22",
+    },
+    {
+        title:"bfdfda",
+        rating:"133.22",
+    },
+    {
+        title:"bfdfda",
+        rating:"133.22",
+    },
+    {
+        title:"bfdfda",
+        rating:"133.22",
+    },
+    {
+        title:"bfdfda",
+        rating:"133.22",
+    },
+    
+    
     
 
 ]
@@ -33,16 +55,39 @@ card.classList ="card";
 
 
 const moviecard = `
+<div class="card ">
 <div class="card-body">
-<h5 class="card-title">${data[index].title}</h5>
-<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-<p class="card-text">Some ${data[index].rating} quick example text to build on the card title and make up the bulk of the card's content.</p>
-<a href="#" class="card-link">Card link</a>
-<a href="#" class="card-link">Another link</a>
-</div>
 
+    <i class="bi bi-calendar-event"></i>
+    <span>Posted on:16-Nov-22</span>
 
-`;
+    <div class="dofix">
+        <i class="bi bi-person-fill"></i>
+        <span style="font-size:20px;color: rgb(77, 92, 221);">Wanted Home Tutor For...</span>
+
+    </div>
+    <div>
+        <i class="bi bi-geo-alt"></i>
+        <span>Gyoti nagar west delhi</span>
+    </div>
+    <div>
+        <span style="color: brown">Prefered :</span>
+        <i class="bi bi-person-circle"></i>Any <i class="bi bi-watch"></i><span>Evening</span>
+        <hr>
+
+    </div>
+    <div><i class="bi bi-file-earmark-break"></i><span>1st class TuitionId: 556341</span></div>
+    <hr>
+    <div>
+        <pre>  <i class="bi bi-book"></i> :<span>All</span></pre>
+    </div>
+    <hr>
+    <div style="background-color:lightblue;padding:20px;border-radius: 10px;">Wanted home tutor for 1st
+        class, All</div>
+    <button style="margin-top:8px;border-radius: 10px;background-color: rgb(10, 172, 236);"> <span
+            style="color: white">view Details</span> </button>
+`
+
 
 
 card.innerHTML += moviecard;
